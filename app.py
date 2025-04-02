afc3c7bda44b53cfcff82fd77b2dc868a5bce873
HEMANTH REDDY MUNAGALA <137473414+hemanthreddy1888@users.noreply.github.com>
2025-04-02 13:06:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 import base64
 import io
 import runpod
+from pydantic import BaseModel
 from diffusers import FluxTransformer2DModel, FluxPipeline, AutoencoderKL
 from transformers import T5EncoderModel, CLIPTextModel
 from src.pipeline_tryon import FluxTryonPipeline
